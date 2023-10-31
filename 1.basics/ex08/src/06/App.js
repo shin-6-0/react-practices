d0ba20ba961deced7d17f28f2c12ec5d5dbb1849
shin-6-0 <syk6715@naver.com>
2023-10-31 16:29:33 +0900

    Component #1

---
+++ 1.basics/ex08/src/06/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import Contents from './Contents';
+
+function App() {
+    return (
+        <div id='App'>
+            <Header />
+            <Contents />            
+        </div>
+    );
+}
+
+export {App};
+
+/*
+Single Root 란 : 컴포넌트 리턴ㄴ할 때는 1개만 리턴해야 된다는 것! 
+<></> 를 써서라도 꼭 한개만 리턴하기
+*/
