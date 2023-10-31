8772c5692950079c860f3c0ae438d8de7691f30e
shin-6-0 <syk6715@naver.com>
2023-10-31 16:23:37 +0900

    jsx #6

---
+++ 1.basics/ex08/src/04/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Contents from './Contents';
+
+const str = 'Hello World';
+function App() {
+    return React.createElement('div',null, React.createElement(Header,null), React.createElement(Contents,null));
+}
+
+export {App};
+
+/*
+Single Root 란 : 컴포넌트 리턴ㄴ할 때는 1개만 리턴해야 된다는 것! 
+<></> 를 써서라도 꼭 한개만 리턴하기
+*/
